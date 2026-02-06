f0f0eca1412dad3aa5b1b1ab1c18bf6fd80fe3a4
yash <yashrajsinhjadeja2253@gmail.com>
2026-02-06 17:25:39 +0530

    Drawer setup complete
    - Drawer setup done
    - It opens and let user pick the category
    - App icon added
    - LaunchScreen icon added

--- Drawer/Controllers/ViewController.swift
+++ Drawer/Controllers/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     private let sideMenuWidth: CGFloat = 260
     private var isExpanded = false
 
+    //To dim the view
+    private var dimmingView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpSideView()
@@ -29,6 +32,7 @@ class ViewController: UIViewController {
 
         sideMenuViewController.delegate = self
 
+        // 1️⃣ Add side menu first
         addChild(sideMenuViewController)
         view.addSubview(sideMenuViewController.view)
         sideMenuViewController.didMove(toParent: self)
@@ -36,11 +40,10 @@ class ViewController: UIViewController {
         sideMenuViewController.view.translatesAutoresizingMaskIntoConstraints =
             false
 
-        //constraint we animate
         sideMenuLeadingConstraint = sideMenuViewController.view.leadingAnchor
             .constraint(
                 equalTo: view.leadingAnchor,
-                constant: -sideMenuWidth  // hidden initially
+                constant: -sideMenuWidth
             )
 
         NSLayoutConstraint.activate([
@@ -51,32 +54,107 @@ class ViewController: UIViewController {
             sideMenuViewController.view.bottomAnchor.constraint(
                 equalTo: view.bottomAnchor
             ),
-            // width already comes from storyboard (260)
+            sideMenuViewController.view.widthAnchor.constraint(
+                equalToConstant: sideMenuWidth
+            ),
         ])
 
-    }
+        // Add dimming view ABOVE main view but BELOW menu
+        dimmingView = UIView()
+        dimmingView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        dimmingView.alpha = 0
+        dimmingView.translatesAutoresizingMaskIntoConstraints = false
 
-    @IBAction func btnAnimate(_ sender: UIButton) {
-        isExpanded.toggle()
-        sideMenuLeadingConstraint.constant = isExpanded ? 0 : -self.sideMenuWidth
-        
-        UIView.animate(withDuration: 0.3) {
+        view.insertSubview(
+            dimmingView,
+            belowSubview: sideMenuViewController.view
+        )
+
+        NSLayoutConstraint.activate([
+            dimmingView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            dimmingView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            dimmingView.topAnchor.constraint(equalTo: view.topAnchor),
+            dimmingView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+
+        // Tap to close menu
+        let tapGesture = UITapGestureRecognizer(
+            target: self,
+            action: #selector(closeMenu)
+        )
+        dimmingView.addGestureRecognizer(tapGesture)
+
+    }
+    //MARK: Close menu
+    @objc func closeMenu() {
+        isExpanded = false
+        sideMenuLeadingConstraint.constant = -sideMenuWidth
+
+        UIView.animate(
+            withDuration: 0.25,
+            delay: 0,
+            options: .curveEaseIn
+        ) {
+            self.dimmingView.alpha = 0
             self.view.layoutIfNeeded()
         }
-        
+    }
+
+    //MARK: To open menu with animations
+    @objc func openMenu() {
+        isExpanded = true
+        sideMenuLeadingConstraint.constant = 0
+
+        UIView.animate(
+            withDuration: 0.35,
+            delay: 0,
+            usingSpringWithDamping: 0.85,
+            initialSpringVelocity: 0.5,
+            options: .curveEaseOut
+        ) {
+            self.dimmingView.alpha = 1
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @IBAction func btnShow(_ sender: UIButton) {
+        isExpanded ? closeMenu() : openMenu()
+    }
+    
+    //MARK: Update button state
+
+    //MARK: open different screen
+    func openScreen(id: String) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let newVC = storyboard.instantiateViewController(withIdentifier: id)
+
+        addChild(newVC)
+        newVC.view.frame = view.bounds
+        view.insertSubview(
+            newVC.view,
+            belowSubview: dimmingView
+        )
+        newVC.didMove(toParent: self)
     }
 }
 extension ViewController: SideMenuViewControllerDelegate {
     func selectedCell(_ row: Int) {
+        closeMenu()
         // close menu after selection
         switch row {
         case 0:
-            print("Home")
-        default :
+            openScreen(id: "HomeController")
+        case 1:
+            openScreen(id: "RPGController")
+        case 2:
+            openScreen(id: "RaceController")
+        case 3:
+            openScreen(id: "GunController")
+        case 4:
+            openScreen(id: "3DController")
+        default:
             break
         }
     }
-    
-    
-    
+
 }
